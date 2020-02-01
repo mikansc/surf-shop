4eac7bf3dabf06b9439f8c3b426635fb178ad5a0
Michael Nascimento <michael.nsc@outlook.com>
2020-02-01 04:50:11 -0300

    Update routes, create models

--- app.js
+++ app.js
@@ -3,11 +3,11 @@ const express = require("express");
 const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
+const bodyParser = require("body-parser");
 
-const indexRouter = require("./routes/index");
-const usersRouter = require("./routes/users");
-const postsRouter = require("./routes/posts");
-const reviewsRouter = require("./routes/reviews");
+const index = require("./routes/index");
+const posts = require("./routes/posts");
+const reviews = require("./routes/reviews");
 
 const app = express();
 
@@ -21,8 +21,9 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use("/", indexRouter);
-app.use("/users", usersRouter);
+app.use("/", index);
+app.use("/posts", posts);
+app.use("/posts/:id/reviews", reviews);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
