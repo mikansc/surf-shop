30dca91d13dc2a9b8082535991cd0736ce1e9473
Michael Nascimento <michael.nsc@outlook.com>
2020-02-19 10:07:11 -0300

    Add forgot pass funcionality

--- models/user.js
+++ models/user.js
@@ -4,13 +4,15 @@ const Schema = mongoose.Schema;
 
 const UserSchema = new Schema({
   name: String,
+  email: { type: String, unique: true, require: true },
   image: {
     secure_url: {
       type: String, default: '/images/default-profile.jpg'
     },
     public_id: String
   },
-  email: { type: String, unique: true, require: true }
+  resetPasswordToken: String,
+  resetPasswordExpires: Date
 });
 
 UserSchema.plugin(passportLocalMongoose);
