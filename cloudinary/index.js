4a767764a941605ddf66d5ccdce709df60437826
Michael Nascimento <michael.nsc@outlook.com>
2020-06-26 15:35:23 -0300

    Few lint fixes

--- cloudinary/index.js
+++ cloudinary/index.js
@@ -1,22 +1,25 @@
-const crypto = require("crypto");
-const cloudinary = require("cloudinary");
+const crypto = require('crypto');
+const cloudinary = require('cloudinary');
+
 cloudinary.config({
-  cloud_name: "mkdev",
-  api_key: "397173953613858",
-  api_secret: process.env.CLOUDINARY_SECRET
+  cloud_name: 'mkdev',
+  api_key: '397173953613858',
+  api_secret: process.env.CLOUDINARY_SECRET,
 });
-const cloudinaryStorage = require("multer-storage-cloudinary");
+
+const cloudinaryStorage = require('multer-storage-cloudinary');
+
 const storage = cloudinaryStorage({
   cloudinary,
-  folder: "surf-shop",
-  allowedFormats: ["jpeg", "jpg", "png"],
-  filename: function(req, file, cb) {
+  folder: 'surf-shop',
+  allowedFormats: ['jpeg', 'jpg', 'png'],
+  filename: function (req, file, cb) {
     let buf = crypto.randomBytes(16);
-    buf = buf.toString("hex");
-    let uniqFileName = file.originalname.replace(/\.jpeg|\.jpg|\.png/gi, "");
+    buf = buf.toString('hex');
+    let uniqFileName = file.originalname.replace(/\.jpeg|\.jpg|\.png/gi, '');
     uniqFileName += buf;
     cb(undefined, uniqFileName);
-  }
+  },
 });
 
 module.exports = { cloudinary, storage };
